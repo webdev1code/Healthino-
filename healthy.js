dbfe851fb749d57ba278733996fc5d9cdf5d4638
menna fakhry <mennafakhry437@gmail.com>
2024-12-26 13:36:24 +0200

    Add files via upload

---
+++ healthy.js
@@ -0,0 +1,13 @@
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => {
+        if (page.id === pageId) {
+            page.classList.add('active');
+        } else {
+            page.classList.remove('active');
+        }
+    });
+}
+
+// Initialize the default page
+document.getElementById('about').classList.add('active');
